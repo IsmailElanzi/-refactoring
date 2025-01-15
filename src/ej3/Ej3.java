759b89d26f3d90177bc4869e5eee06073c3fbb89
Silvia Mª Moreno García <moreno.garcia.silvia.maria@iescamas.es>
2025-01-15 12:29:02 +0100

    Refactorización del Ej3

--- src/ej3/Ej3.java
+++ src/ej3/Ej3.java
@@ -1,25 +1,20 @@
 package ej3;
 
 public class Ej3 {
-	
-	 private static boolean esConsonante(char letra) {
-	        if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' 
-	            || letra == 'u' || letra == 'A' || letra == 'E' || letra == 'I' 
-	            || letra == 'O' || letra == 'U') {
-	            return true;
-	        } else {
-	            return false;
-	        }
-	    }
 
-	    public static void main(String[] args) {
-	        if (esConsonante(letra) == true) {
-	            System.out.println("la letra " + letra + " es una vocal");
-	        } else {
-	            System.out.println("la letra " + letra + " no es una vocal");
-	        }
+	private static boolean esVocal(char letra) {
+        return letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' 
+            || letra == 'u' || letra == 'A' || letra == 'E' || letra == 'I' 
+            || letra == 'O' || letra == 'U';
+    }
 
-	        char letra = 'B';
-	    }
-
+    public static void main(String[] args) {
+        char letra = 'B';
+        
+        if (esVocal(letra)) {
+            System.out.println("La letra " + letra + " es una vocal");
+        } else {
+            System.out.println("La letra " + letra + " no es una vocal");
+        }
+    }
 }
